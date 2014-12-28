64908592ad4ce0d41b323c449dd690ea7adb3736
Joel Burton <joel@joelburton.com>
2014-12-27 20:53:15 -0500

    Update deployment settings.

--- project/otessier/settings/production.py
+++ project/otessier/settings/production.py
@@ -9,13 +9,13 @@ from .base import *
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['new.otessier.com', 'otessier.com']
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'otessier',
-        'HOST': 'localhost',
+        'HOST': 'lekman.joelburton.com',
         'USER': 'otessier',
         'PASSWORD': os.environ['PG_PASSWORD'],
         'CONN_MAX_AGE': None,
