a5027ea629e29805fb5367bd012007682b1c231c
Joel Burton <joel@joelburton.com>
2015-01-05 23:07:36 -0800

    Add backup script.

--- conf/cron.daily/otessier.com-backup.sh
+++ conf/cron.daily/otessier.com-backup.sh
@@ -3,3 +3,4 @@
 cd /home/django/otessier.com/
 venv/bin/python project/manage.py dbbackup
 venv/bin/python project/manage.py mediabackup
+s3cmd put /home/django/otessier.com/backups/* s3://otessier-com-backup
